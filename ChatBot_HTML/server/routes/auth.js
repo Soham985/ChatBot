ea37e79273609d08edf2bd2f9d0fe12aa94193ac
Soham985 <sohamguhathakurata1999@gmail.com>
2020-06-23 21:42:36 +0530

    first commit

---
+++ ChatBot_HTML/server/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router  = express.Router()
+
+const AuthController = require('../controllers/AuthController')
+const ChatController = require('../controllers/ChatController')
+//const authenticate   = require('../middleware/authentication')
+
+router.post('/register', AuthController.register)
+router.post('/login',AuthController.login)
+router.post('/chatHistory',ChatController.registerChat)
+router.post('/updateChat',ChatController.updateChat)
+
+module.exports=router
